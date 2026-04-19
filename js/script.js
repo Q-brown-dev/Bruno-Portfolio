65c1253f6b75a7656537e2defa101f4738fd3fd8
unknown <anyikwabruno1@.com>
2026-04-19 14:09:02 -0700

    Update js/

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if (cardTop < triggerBottom) {
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
